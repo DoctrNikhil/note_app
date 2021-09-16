ede18426c8a957ccbcdb1b33d53fe819bcdb268d
Nikhil Kumar <airmailtonikhil@gmail.com>
2021-09-16 21:38:07 +0530

    Added node_modules

---
+++ build/precache-manifest.9942268a2516c440c5ee80383942bc0d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee96f8a0abf346187853d08ddbf20f2",
+    "url": "/Note_Taking_React_App/index.html"
+  },
+  {
+    "revision": "0518db8b7925987642f2",
+    "url": "/Note_Taking_React_App/static/js/2.d5cd440c.chunk.js"
+  },
+  {
+    "revision": "d5331c4ccd2ec53b50a1",
+    "url": "/Note_Taking_React_App/static/js/main.12c55cd3.chunk.js"
+  },
+  {
+    "revision": "eba4954e766a810e36e6",
+    "url": "/Note_Taking_React_App/static/js/runtime-main.1efc565d.js"
+  }
+]);
