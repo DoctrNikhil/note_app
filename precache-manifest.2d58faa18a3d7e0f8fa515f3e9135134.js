d071bf3c563a30070f1ce41d051f6b5c8a942d81
DoctrNikhil <airmailtonikhil@gmail.com>
2021-09-17 06:32:58 +0530

    Updates

---
+++ precache-manifest.2d58faa18a3d7e0f8fa515f3e9135134.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062c07eee20742703e13e1b9704c0dda",
+    "url": "/note_app/index.html"
+  },
+  {
+    "revision": "0518db8b7925987642f2",
+    "url": "/note_app/static/js/2.d5cd440c.chunk.js"
+  },
+  {
+    "revision": "d5331c4ccd2ec53b50a1",
+    "url": "/note_app/static/js/main.12c55cd3.chunk.js"
+  },
+  {
+    "revision": "d4ac361e4ffd97069f4c",
+    "url": "/note_app/static/js/runtime-main.a0538ee7.js"
+  }
+]);
